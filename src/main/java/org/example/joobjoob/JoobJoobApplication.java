e8f8522aba6df63f6555d17f53c1ae217eeb775e
dongyuppen <dongyuppen@naver.com>
2025-09-18 16:55:33 +0900

    first commit

---
+++ src/main/java/org/example/joobjoob/JoobJoobApplication.java
@@ -0,0 +1,13 @@
+package org.example.joobjoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoobJoobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoobJoobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/joobjoob/JoobJoobApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.joobjoob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoobJoobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
